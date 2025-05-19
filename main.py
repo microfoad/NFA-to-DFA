4622ca622f3d4400ae14b6713d00b98cca2f1b93
MrRobot <foad.programmer@gmail.copm>
2025-05-19 16:26:22 +0330

    new changes happend

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sys
 from nazarya import NFA, DFA
 
 
-
 if sys.version_info >= (3, 0):
     filename = input('Enter the name of the NFA file: ')
 elif sys.version_info >= (2, 0):
@@ -27,4 +26,32 @@ dfa = DFA()
 nfa.construct_nfa_from_file(lines)
 dfa.convert_from_nfa(nfa)
 
+#---------------------------------
+def main():
+    # Get filename from user input
+    if sys.version_info >= (3, 0):
+        filename = input('Enter the name of the NFA file: ')
+    elif sys.version_info >= (2, 0):
+        filename = raw_input('Enter the name of the NFA file: ')
+    else:
+        print("Please update python to version 2.0 or newer")
+        quit()
+
+    # Read the file
+    file = open(filename, 'r')
+    lines = file.readlines()
+    file.close()
+
+    # Create and process NFA and DFA
+    nfa = NFA()
+    dfa = DFA()
+
+    nfa.construct_nfa_from_file(lines)
+    dfa.convert_from_nfa(nfa)
+    dfa.print_dfa()
+
+if __name__ == "__main__":
+    main()
+
+#------------------------------
 dfa.print_dfa()
